cf93734046b69e924d71072c2d7ae813cc554e7c
James Andrews <cjand225@gmail.com>
2023-10-05 14:59:42 -0600

    Added missing docstrings for controller component

--- app/tests/controller/test_exchange_rate_controller.py
+++ app/tests/controller/test_exchange_rate_controller.py
@@ -19,14 +19,32 @@ from fastapi.testclient import TestClient
 client = TestClient(app)
 
 def mock_fetch_exchange_rates_success(*args, **kwargs):
+    """
+    Mock function to simulate a successful fetch of exchange rates from the service layer.
+
+    Returns:
+        tuple: A tuple containing exchange rate data and None for the error.
+    """
     return {'2023-08-01': 1.25, '2023-08-02': 1.26}, None
 
 
 def mock_fetch_exchange_rates_failure(*args, **kwargs):
+    """
+    Mock function to simulate a failure in fetching exchange rates from the service layer.
+
+    Returns:
+        tuple: A tuple containing None for the exchange rate data and an error message.
+    """
     return None, "Some error occurred"
 
 
 def test_get_exchange_rates_success():
+    """
+    Test the /exchange_rates/ endpoint for a successful response.
+    
+    The service layer is mocked to return a successful response. The test then checks if the API endpoint
+    returns the expected status code and JSON response.
+    """
     with patch('app.api.service.exchange_rate_service.ExchangeRateService.fetch_exchange_rates', new=mock_fetch_exchange_rates_success):
         response = client.get("/exchange_rates/")
         assert response.status_code == 200
@@ -37,6 +55,12 @@ def test_get_exchange_rates_success():
 
 
 def test_get_exchange_rates_failure():
+    """
+    Test the /exchange_rates/ endpoint for a failure scenario.
+    
+    The service layer is mocked to return an error. The test then checks if the API endpoint
+    returns the expected status code and error message in the JSON response.
+    """
     with patch('app.api.service.exchange_rate_service.ExchangeRateService.fetch_exchange_rates', new=mock_fetch_exchange_rates_failure):
         response = client.get("/exchange_rates/")
         assert response.status_code == 500
