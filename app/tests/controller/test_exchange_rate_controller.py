23a4bbebff37bacd2f9eb38d3509deeb4bb13858
James Andrews <cjand225@gmail.com>
2023-10-05 14:55:34 -0600

    Updated tests and enhanced docs for service/controller components

--- app/tests/controller/test_exchange_rate_controller.py
+++ app/tests/controller/test_exchange_rate_controller.py
@@ -1,9 +1,22 @@
-from app.main import app
-from fastapi.testclient import TestClient
+"""
+test_exchange_rate_controller.py
+
+This module contains tests for the exchange_rate_controller module. It tests the behavior of the API endpoint
+by mocking the service layer and checking the responses from the controller.
+"""
+
+import os
 from unittest.mock import patch
 
-client = TestClient(app)
+# Set the environment variable for testing purposes.
+os.environ["ENV"] = "development"
 
+# Now, import the app after setting the environment variable.
+from app.main import app
+from fastapi.testclient import TestClient
+
+# Initialize a test client for the FastAPI application.
+client = TestClient(app)
 
 def mock_fetch_exchange_rates_success(*args, **kwargs):
     return {'2023-08-01': 1.25, '2023-08-02': 1.26}, None
--- app/tests/service/test_exchange_rate_service.py
+++ app/tests/service/test_exchange_rate_service.py
@@ -1,8 +1,21 @@
+"""
+test_exchange_rate_service.py
+
+This module contains tests for the ExchangeRateService class. It tests the behavior of the service
+by mocking external HTTP requests and checking the responses from the service methods.
+"""
+
 from app.api.service.exchange_rate_service import ExchangeRateService
 from unittest.mock import patch
 
-
 def successful_mock_response(*args, **kwargs):
+    """
+    Mock function to simulate a successful HTTP response from the Bank of Canada API.
+
+    Returns:
+        MockResponse: A mock object with a status code of 200 and a json method that returns
+                      exchange rate data.
+    """
     class MockResponse:
         status_code = 200
 
@@ -16,21 +29,34 @@ def successful_mock_response(*args, **kwargs):
             }
     return MockResponse()
 
-
 def failed_mock_response(*args, **kwargs):
+    """
+    Mock function to simulate a failed HTTP response from the Bank of Canada API.
+
+    Returns:
+        MockResponse: A mock object with a status code of 500.
+    """
     class MockResponse:
         status_code = 500
     return MockResponse()
 
-
 def test_fetch_exchange_rates_success():
+    """
+    Test the fetch_exchange_rates method for a successful response.
+    
+    The external HTTP request is mocked to return a successful response.
+    """
     with patch('requests.get', new=successful_mock_response):
         rates, error = ExchangeRateService.fetch_exchange_rates()
         assert error is None
         assert rates == {'2023-08-01': 1.25, '2023-08-02': 1.26}
 
-
 def test_fetch_exchange_rates_failure():
+    """
+    Test the fetch_exchange_rates method for a failure scenario.
+    
+    The external HTTP request is mocked to return an error.
+    """
     with patch('requests.get', new=failed_mock_response):
         rates, error = ExchangeRateService.fetch_exchange_rates()
         assert rates is None
