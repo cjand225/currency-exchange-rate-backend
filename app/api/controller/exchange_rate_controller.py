192f69611b2bb35d799fb53c5c3c7dd8553893cf
James Andrews <cjand225@gmail.com>
2023-10-05 14:50:50 -0600

    Enhanced documentation for service/controller components

--- app/api/controller/exchange_rate_controller.py
+++ app/api/controller/exchange_rate_controller.py
@@ -1,8 +1,16 @@
+"""
+exchange_rate_controller.py
+
+This module defines the API endpoints related to exchange rates. It uses the ExchangeRateService
+to fetch exchange rate data and provides an endpoint to retrieve exchange rates based on given parameters.
+"""
+
 from fastapi import APIRouter, Query, status
 from fastapi.responses import JSONResponse
 from typing import Annotated
 from app.api.service.exchange_rate_service import ExchangeRateService
 
+# Create a new router instance for the exchange rate endpoints.
 router = APIRouter()
 
 
@@ -24,16 +32,32 @@ def get_exchange_rates(
         str | None,
         Query(description="End date range for Exchange Rate (format YYYY-MM-DD)"),
     ] = "2023-08-31",
-):
+) -> JSONResponse:
+    """
+    Fetch exchange rates based on the provided parameters.
+
+    Args:
+        from_currency (str, optional): The source currency for the exchange rate. Defaults to "USD".
+        to_currency (str, optional): The target currency for the exchange rate. Defaults to "CAD".
+        start_date (str, optional): The start date for the exchange rate data range. Defaults to "2023-08-01".
+        end_date (str, optional): The end date for the exchange rate data range. Defaults to "2023-08-31".
+
+    Returns:
+        JSONResponse: A JSON response containing the exchange rate data or an error message.
+    """
+
+    # Fetch exchange rate data using the ExchangeRateService.
     exchange_rate, error = ExchangeRateService.fetch_exchange_rates(
         from_currency, to_currency, start_date, end_date)
 
+    # Handle potential errors from the service.
     if error:
         return JSONResponse(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             content={"error": error}
         )
 
+    # Return the exchange rate data if available.
     if len(exchange_rate) > 0:
         return JSONResponse(
             status_code=status.HTTP_200_OK,
@@ -42,6 +66,7 @@ def get_exchange_rates(
                 "exchange_rate": exchange_rate
             }
         )
+    # Handle the case where no exchange rate data is found for the given date range.
     else:
         return JSONResponse(
             status_code=status.HTTP_404_NOT_FOUND,
--- app/api/service/exchange_rate_service.py
+++ app/api/service/exchange_rate_service.py
@@ -3,6 +3,9 @@ import requests
 
 
 class ExchangeRateService:
+    """
+    Service class responsible for fetching exchange rate data from the Bank of Canada API.
+    """
 
     @staticmethod
     def fetch_exchange_rates(
@@ -10,13 +13,32 @@ class ExchangeRateService:
         to_currency: str = "CAD",
         start_date: str = "2023-08-01",
         end_date: str = "2023-08-31",
-    ):
+    ) -> (dict, str):
+        """
+        Fetch exchange rates for a given currency pair and date range.
+
+        Args:
+            from_currency (str, optional): The source currency for the exchange rate. Defaults to "USD".
+            to_currency (str, optional): The target currency for the exchange rate. Defaults to "CAD".
+            start_date (str, optional): The start date for the exchange rate data range. Defaults to "2023-08-01".
+            end_date (str, optional): The end date for the exchange rate data range. Defaults to "2023-08-31".
+
+        Returns:
+            tuple: A tuple containing two elements:
+                - dict: A dictionary with dates as keys and exchange rates as values.
+                - str: An error message if there was an issue fetching the data, otherwise None.
+        """
+
+        # Construct the API request URL using the provided parameters.
         base_url = "https://www.bankofcanada.ca/valet/observations/"
         serie_name = f"FX{from_currency}{to_currency}"
         api_url = f"{base_url}{serie_name}?start_date={start_date}&end_date={end_date}"
 
         try:
+            # Make an HTTP GET request to the Bank of Canada API.
             response = requests.get(api_url)
+
+            # Check if the response status code is successful (200 OK).
             if response.status_code == 200:
                 data = response.json()
                 exchange_rate = {}
